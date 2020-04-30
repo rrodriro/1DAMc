a14edd4f17aa5ea97da3790090c19e4b29d4aac5
Rosana <Rosana@DESKTOP-LI5B5FL>
2020-04-30 08:49:26 +0200

    con mi nombre

--- EjemploGit/src/ejemploGit/EjempliGit.java
+++ EjemploGit/src/ejemploGit/EjempliGit.java
@@ -5,6 +5,7 @@ public class EjempliGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Primer ejemplo Git");
+		System.out.println("Soy Rosana");
 	}
 
 }
