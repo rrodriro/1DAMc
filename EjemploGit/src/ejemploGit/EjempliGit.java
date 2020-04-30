4c14a5c572e7e924813f5644eedc49786fc1e58a
Rosana <Rosana@DESKTOP-LI5B5FL>
2020-04-30 08:43:20 +0200

    primer ejemplo

---
+++ EjemploGit/src/ejemploGit/EjempliGit.java
@@ -0,0 +1,10 @@
+package ejemploGit;
+
+public class EjempliGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Primer ejemplo Git");
+	}
+
+}
